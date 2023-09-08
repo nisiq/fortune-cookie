c208dadf76c336fda69f5a8af898209b185d10b8
nisiq <nicole.siq2018@gmail.com>
2023-09-08 16:14:03 -0300

    settings

--- App.js
+++ App.js
@@ -1,114 +1,101 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-
-class App extends Component{
-
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       textoFrase: 'Ataque é veneno, e veneno só funciona se eu tomar.',
-      img: require('./assets/biscoito.png')
+      img: require('./assets/biscoito.png'),
     };
 
-    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
 
-    this.frases = [
-      'Se a educação não for provocativa, não constrói, não se cria, não se inventa, só se repete.',
-      'Só um imbecil gostaria de fazer o que não gosta.',
-      'Cuidado com gente que concorda contigo o tempo todo!',
-      'Quem sabe, divide. Quem não sabe, pergunta.',
-      'Tem gente que esquece que você não precisa diminuir o outro para crescer.',
-      'O ataque pessoal é a falência da razão.',
-      'O que todo estado fundamentalista teme é a educação das pessoas.',
-      'É você quem vai fazer o seu diploma, ninguém vai fazer por você.',
-      'É no conhecimento que existe uma chance de libertação. ',
-      'Ataque é veneno, e veneno só funciona se eu tomar.'
-    ];
+    this.frases = [ 'Se a educação não for provocativa, não constrói, não se cria, não se inventa, só se repete.',
+                    'Só um imbecil gostaria de fazer o que não gosta.', 'Cuidado com gente que concorda contigo o tempo todo!',
+                    'Quem sabe, divide. Quem não sabe, pergunta.', 'Tem gente que esquece que você não precisa diminuir o outro para crescer.',
+                    'O ataque pessoal é a falência da razão.', 'O que todo estado fundamentalista teme é a educação das pessoas.',
+                    'É você quem vai fazer o seu diploma, ninguém vai fazer por você.', 'É no conhecimento que existe uma chance de libertação. ',
+                    'Ataque é veneno, e veneno só funciona se eu tomar.' 
+                  ];
+
   }
 
-  quebraBiscoito(){
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length );
+  quebraBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
 
     this.setState({
-      textoFrase: ' "' + this.frases[numeroAleatorio] + '"',
-      img: require('./assets/biscoitoAberto.png')
+      img: require('./assets/biscoitoAberto.png'),
     });
 
-     //definir atraso para o biscoito fechar dnv
-     setTimeout(() => {
+    // Define um atraso de 1 segundo para exibir a nova frase
+    setTimeout(() => {
       this.setState({
-        textoFrase: '',
-        img: require('./assets/biscoito.png'),
+        textoFrase: ' "' + this.frases[numeroAleatorio] + '"',
       });
-    }, 4000);
+    }, 1000);
+
+    // Define um segundo atraso de 2 segundos para voltar à imagem fechada e limpar a frase
+    setTimeout(() => {
+      this.setState({
+        img: require('./assets/biscoito.png'),
+        textoFrase: '',
+      });
+    }, 3000);
   }
 
-
-  
-  render(){
+  render() {
     return (
       <View style={styles.container}>
-
-        <Image
-          source={this.state.img}
-          style={styles.img}
-
-        />
-
+        <Image source={this.state.img} style={styles.img} />
         <Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
-
         <TouchableOpacity style={styles.botao} onPress={this.quebraBiscoito}>
-
           <View style={styles.btnArea}>
             <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
           </View>
-
         </TouchableOpacity>
-
       </View>
-
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     paddingTop: 20,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#000'
+    backgroundColor: '#000',
   },
-  img:{
+  img: {
     width: 250,
     height: 250,
   },
-  textoFrase:{
+  textoFrase: {
     fontSize: 20,
     color: '#fff',
     margin: 30,
     fontStyle: 'italic',
-    textAlign: 'center'
+    textAlign: 'center',
   },
-  botao:{
+  botao: {
     width: 230,
     height: 50,
     borderWidth: 2,
     borderColor: '#fff',
-    borderRadius: 25
+    borderRadius: 25,
   },
-  btnArea:{
-    flex:1,
+  btnArea: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  btnTexto:{
+  btnTexto: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#fff'
-  }
+    color: '#fff',
+  },
 });
 
-export  default  App;
+export default App;
